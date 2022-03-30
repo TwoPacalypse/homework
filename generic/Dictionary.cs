9123a66a6b4049b19fdb44cfc2794b6205c8b60c
TwoPacalypse <mihail22010@yandex.ru>
2022-03-30 16:00:58 +0300

    Добавьте файлы проекта.

---
+++ generic/Dictionary.cs
@@ -0,0 +1,56 @@
+﻿class DictClass
+{
+    private static void Main()
+    {
+        Dictionary<int, List<Entity>> resultDict = new Dictionary<int, List<Entity>>();
+
+        Entity first = new Entity(1, 0, "Root entity");
+        Entity second = new Entity(2, 1, "Child of 1 entity");
+        Entity third = new Entity(3, 1, "Child of 1 entity");
+        Entity fourth = new Entity(4, 2, "Child of 2 entity");
+        Entity fifth = new Entity(5, 4, "Child of 4 entity");
+
+        List<Entity> entityList = new List<Entity>();
+        entityList.Add(first);
+        entityList.Add(second);
+        entityList.Add(third);
+        entityList.Add(fourth);
+        entityList.Add(fifth);
+
+        first.ToDict(entityList, resultDict);
+
+        foreach (var gr in resultDict)
+        {
+            Console.Write($"Key: {gr.Key},  Value = List: ");
+            foreach (var x in gr.Value)
+            {
+                Console.Write($"Entity [id = {x.Id}] ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+public class Entity
+{
+    public int Id { get; set; }
+    public int ParentId { get; set; }
+    public string Name { get; set; }
+
+    public Entity(int id, int parentid, string name)
+    {
+        Id = id;
+        ParentId = parentid;
+        Name = name;
+    }
+
+    public void ToDict(List<Entity> entityList, Dictionary<int, List<Entity>> dict)
+    {
+        var result = entityList.GroupBy(p => p.ParentId);
+        foreach (var group in result)
+        {
+            var list = group.ToList();
+            dict.Add(group.Key, list);
+        }
+    }
+}
---
+++ generic/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generic
+{
+    class Program
+    {
+        private static void Main()
+        {
+            LocalFileLogger<string> infoTest = new LocalFileLogger<string>();
+            infoTest.Message = "test test test";
+            infoTest.LogInfo(infoTest.Message);
+
+            LocalFileLogger<int> warningTest = new LocalFileLogger<int>();
+            warningTest.Message = 45;
+            warningTest.LogWarning(warningTest.Message);
+
+            LocalFileLogger<int> errorTest = new LocalFileLogger<int>();
+            try
+            {
+                errorTest.Message = 5;
+                int y = errorTest.Message / 0;
+            }
+            catch (Exception ex)
+            {
+                errorTest.LogError(errorTest.Message, ex);
+            }
+        }
+    }
+
+    public interface ILogger<T>
+    {
+        T Message { get; set; }
+
+        public void LogInfo(T message);
+        public void LogWarning(T message);
+        public void LogError(T message, Exception ex);
+    }
+
+    public class LocalFileLogger<T> : ILogger<T>
+    {
+        public StreamWriter f = new StreamWriter("..\\..\\..\\test.txt", true);
+        string? GenericTypeName { get; set; }
+        public LocalFileLogger()
+        {
+            GenericTypeName = typeof(T).Name;
+        }
+        public T? Message { get; set; }
+        public void LogInfo(T message)
+        {
+            f.WriteLine($"[Info]: [{GenericTypeName}] : {message}");
+            f.Close();
+        }
+        public void LogWarning(T message)
+        {
+            f.WriteLine($"[Warning]: [{GenericTypeName}] : {message}");
+            f.Close();
+        }
+        public void LogError(T message, Exception ex)
+        {
+            f.WriteLine($"[Error]: [{GenericTypeName}] : {message}. {ex.Message}");
+            f.Close();
+        }
+    }
+}
+
